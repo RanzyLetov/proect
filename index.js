c0112467d260d48a395c91f5ac833bfc4516bb43
RanzyLetov <viktorkravcenko171@gmail.com>
2026-04-30 23:39:11 +0700

    simplify cors

--- index.js
+++ index.js
@@ -8,25 +8,9 @@ const pool = require('./db')
 
 const app = express()
 
-app.use(cors())
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS'
-  )
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200)
-  }
-
-  next()
-})
+app.use(cors({
+  origin: '*'
+}))
 
 app.use(express.json())
 
